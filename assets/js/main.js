5f13c1ca9e2c031ee080d3f619052f764911f919
James Wilkinson <hsvusa@gmail.com>
2019-03-21 09:56:55 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,8 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/1.jpg': 'center',
+							'images/2.jpg': 'center',
 						},
 
 					// Delay.
